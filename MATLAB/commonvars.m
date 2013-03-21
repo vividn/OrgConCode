65d2ce1b7dad36d9773e5e26c2f39c43b389b438
Nate Faber <fabe0088@umn.edu>
2013-03-21 15:37:07 -0500

    Updated paths

--- MATLAB/commonvars.m
+++ MATLAB/commonvars.m
@@ -4,7 +4,7 @@ function commonvars
 
 global ROOT_DIR HOME_DIR DATA_DIR ANALYSIS_DIR Joints PC_AVG PC_COEFFS
 ROOT_DIR = 'C:\OrgCon\';
-HOME_DIR = [ROOT_DIR 'MATLAB\'];
+HOME_DIR = [ROOT_DIR 'OrgConCode\MATLAB\'];
 DATA_DIR = [ROOT_DIR 'ExperimentData\'];
 ANALYSIS_DIR = [ROOT_DIR 'Analysis\'];
 
