e8d28489d47c8c6fc0ca1b2b9b1f475e116fb87a
Nate Faber <fabe0088@umn.edu>
2013-03-28 18:43:36 -0500

    Added function thresholdindex
    Finds the first and last time point in a matrix where the value exceeds
    a certain threshold.

---
+++ MATLAB/thresholdindex.m
@@ -0,0 +1,22 @@
+function [firstIndex lastIndex] = thresholdindex(M,threshold,dim)
+% Finds the first/last index in M where the value exceeds a threshold value
+% [firstIndex lastIndex] = thresholdindex(M,threshold)
+% first index is the first time (or times if multidimensional) where the
+% value of M exceeds the threshold along dimension dim.
+
+if nargin < 3
+    dim = 1;
+end
+
+%Get rid of NaN's if function nonan exists
+if which('nonan')
+    M = nonan(M,dim);
+end
+
+thresholdExceded = M > threshold;
+
+%First time the threshold is exceeded
+[~,firstIndex] = max(thresholdExceded,[],dim);
+
+%Last time the threshold is exceeded
+[~,lastIndex] = max(flipdim(thresholdExceded,dim),[],dim);
