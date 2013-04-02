c878e63e429040d509fdecd46baa0cc5ce94d95b
Nate Faber <fabe0088@umn.edu>
2013-04-02 17:55:19 -0500

    Added function alignpcscores
    Takes a single scene, and uses differential threshold and vector stretch
    to determine where the start and stop times are for each trial and then
    stretch each trial to attempt to align them.

---
+++ MATLAB/alignpcscores.m
@@ -0,0 +1,41 @@
+function alignedPcScores = alignpcscores(SceneStructure,method)
+% Takes the PC scores from a scene, truncates and stretches them to align
+% with each other. Method can be 'max', 'min', or 'avg', or a number:
+%  method determines the size of the time dimension in the returned array
+
+if nargin < 2
+    method = 'avg';
+end
+
+bothHands = cat(2,SceneStructure.Rh.glovePC,SceneStructure.Lh.glovePC);
+[startTrunc stopTrunc] = differentialthreshold(bothHands,0.15,1,2);
+
+switch method
+    case 'max'
+        newSize = max(stopTrunc - startTrunc);
+    case 'min'
+        newSize = min(stopTrunc - startTrunc);
+    case 'avg'
+        newSize = round(mean(stopTrunc - startTrunc));
+    otherwise
+        if isinteger(method) && isscalar(method)
+            newSize = method;
+        else
+            error('alignpcscores:Bad method');
+        end
+end
+
+
+nTrials = size(bothHands,3);
+nComponents = size(bothHands,2);
+
+alignedPcScores = zeros(newSize,nComponents,nTrials); %Initialize
+
+
+for iTrial = 1:nTrials
+    leftTrunc = startTrunc(iTrial);
+    rightTrunc = stopTrunc(iTrial);
+    truncatedData = bothHands(leftTrunc:rightTrunc,:,iTrial);
+    alignedPcScores(:,:,iTrial) = vectorstretch(truncatedData,newSize);
+end
+
