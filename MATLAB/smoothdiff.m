4cc22f75e03f0a67ff52765b989ffaca89947a17
Julia Quindlen <quind003@umn.edu>
2013-04-16 14:00:30 -0500

    edited smoothdiff
    included filter

--- MATLAB/smoothdiff.m
+++ MATLAB/smoothdiff.m
@@ -1,4 +1,4 @@
-function veloc = smoothdiff(M)
+function veloc = smoothdiff(M,k,f)
 %the input to this function should be a nx6x8 Matrix, where n is the number
 %used to smooth the PC data in the 'alignpcscores' function (i.e.
 %M=alignpcscores(MF3(1,scene),n) where 'scene' is the scene number 1-3)
@@ -8,6 +8,7 @@ function veloc = smoothdiff(M)
 %corresponding to the velocity from the left hand.
 veloc=NaN(size(M,1)-1,size(M,3),2);%veloclh=[];
     for nTrial=1:size(M,3)
-            veloc(:,nTrial,1)=sqrt((diff(M(:,1,nTrial))).^2+(diff(M(:,2,nTrial))).^2+(diff(M(:,3,nTrial))).^2);
-            veloc(:,nTrial,2)=sqrt((diff(M(:,4,nTrial))).^2+(diff(M(:,5,nTrial))).^2+(diff(M(:,6,nTrial))).^2);
+        
+            veloc(:,nTrial,1)=sqrt((sgolayfilt(diff(sgolayfilt(M(:,1,nTrial),k,f)),k,f)).^2+(sgolayfilt(diff(sgolayfilt(M(:,2,nTrial),k,f)),k,f)).^2+(sgolayfilt(diff(sgolayfilt(M(:,3,nTrial),k,f)),k,f)).^2);
+            veloc(:,nTrial,2)=sqrt((sgolayfilt(diff(sgolayfilt(M(:,4,nTrial),k,f)),k,f)).^2+(sgolayfilt(diff(sgolayfilt(M(:,5,nTrial),k,f)),k,f)).^2+(sgolayfilt(diff(sgolayfilt(M(:,6,nTrial),k,f)),k,f)).^2);
     end
