b2eeb5220936f7fd0dfe814dc3a151e57fb4b33c
Nate Faber <fabe0088@umn.edu>
2013-05-01 07:36:22 -0500

    Continued work on segmentScene1

--- MATLAB/segmentScene1.m
+++ MATLAB/segmentScene1.m
@@ -1,4 +1,4 @@
-function segmentTimes = segmentScene1(SceneStructure,trial)
+function segmentTimes = segmentScene1(SceneStructure)
 
 
 % Scene 1 Breakdown
@@ -23,48 +23,61 @@ markType = ['lx min';
             'ry max'];
 
 approxTime = [2;6;11;15;19;23;27;33;40];
-         
 
-rhXVals = SceneStructure.Rh.x(:,trial);
-rhYVals = SceneStructure.Rh.y(:,trial);
-lhXVals = SceneStructure.Lh.x(:,trial);
-lhYVals = SceneStructure.Lh.y(:,trial);
+minDist = 2000; %For use with the parameter 'MINPEAKDISTANCE' in findpeaks
 
-minDist = 2000;
+nSegments = min([size(eventNames,1),size(markType,1),size(approxTime,1)]);
+nTrials = size(SceneStructure.trialLengths,1);
 
-for i = 1:length(approxTime)
-    time = approxTime(i)*1000;
-    name = eventNames(i);
+
+for iTrial = 1:nTrials
+    % Initialize outputvariable
+    segmentTimes = zeros(nSegments,nTrials);
     
-    handDimStr = markType(i,1:2);
-    switch handDimStr
-        case 'lx'
-            data = lhXVals;
-        case 'ly'
-            data = lhYVals;
-        case 'rx'
-            data = rhXVals;
-        case 'ry'
-            data = rhYVals;
-        otherwise
-            error('markTypes misconfigured');
+    rhXVals = SceneStructure.Rh.x(:,iTrial);
+    rhYVals = SceneStructure.Rh.y(:,iTrial);
+    lhXVals = SceneStructure.Lh.x(:,iTrial);
+    lhYVals = SceneStructure.Lh.y(:,iTrial);
+    
+    
+    
+    
+    for iSegment = 1:nSegments
+        time = approxTime(iSegment)*1000;
+        name = eventNames(iSegment);
+        
+        handDimStr = markType(iSegment,1:2);
+        switch handDimStr
+            case 'lx'
+                data = lhXVals;
+            case 'ly'
+                data = lhYVals;
+            case 'rx'
+                data = rhXVals;
+            case 'ry'
+                data = rhYVals;
+            otherwise
+                error('markTypes misconfigured');
+        end
+        
+        maxOrMin = markType(iSegment,4:6);
+        switch maxOrMin
+            case 'max'
+                mData = data;
+            case 'min'
+                mData = -data; %Flip data for the peak analysis to find local mins
+            otherwise
+                error('markTypes misconfigured');
+        end
+        
+        [~,indices] = findpeaks(mData,'MINPEAKDISTANCE',minDist);
+        [C,I] = closestval(indices,time);
+        clf
+        plot(data)
+        vline(indices(max(I-2,1):min(I+2,length(indices))),'k--')
+        vline(C,'r-')
+        a = input('0=red line,+1 = one to the right, etc.');
+        if isempty(a),a=0;end
+        segmentTimes(iSegment,iTrial) = indices(I+a);
     end
-    
-    maxOrMin = markType(i,4:6);
-    switch maxOrMin
-        case 'max'
-            mData = data;
-        case 'min'
-            mData = -data; %Flip data for the peak analysis to find local mins
-        otherwise
-            error('markTypes misconfigured');
-    end
-    
-    [~,indices] = findpeaks(mData,'MINPEAKDISTANCE',minDist);
-    [C,I] = closestval(indices,time);
-    clf
-    plot(data)
-    vline(indices(max(I-2,1):min(I+2,length(indices))),'k--')
-    vline(C,'r-')
-    pause
 end
