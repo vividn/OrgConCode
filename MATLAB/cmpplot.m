ce1cd95eb84f59d2d09e1e9a50b06c083aa405a3
Nate Faber <fabe0088@umn.edu>
2013-05-03 11:59:22 -0500

    Readded specify number of columns cmpplot

--- MATLAB/cmpplot.m
+++ MATLAB/cmpplot.m
@@ -1,23 +1,28 @@
-function axisHandleArray = cmpplot(data,dim)
+function axisHandleArray = cmpplot(data,dim,nCols)
 % cmpplot plots each matrix along a dimension on many axes using subplot
+% inputs Data, dim = the dimension which to separate and plot on multiple
+% subplots; cols = the number of columns of subplots.
+% output (if specified) will be an array of the function handles of all the
+% plots.
 
 nDims = length(size(data));
 
-if nargin < 2
-    dim = nDims; %The last dimension
+if nargin < 3
+    nCols = 2;
+    if nargin < 2
+        dim = nDims; %The last dimension
+    end
 end
-
+    
 allDims = 1:nDims;
 otherDims = setdiff(allDims, dim);
 
 permuteData = permute(data,[dim otherDims]); %shifts splitting dim to front 
 
-cols = 2; %Number of columns in the subplot space
-
 p = size(permuteData,1);
-rows = ceil(p/cols);
+rows = ceil(p/nCols);
 for i=1:p
-    axisHandleArray(i) = subplot(rows,cols,i);
+    axisHandleArray(i) = subplot(rows,nCols,i);
     plot(squeeze(permuteData(i,:,:)))
 end
 
