7b0f6f7549435f9243655595457d54a507b0b6db
Nate Faber <fabe0088@umn.edu>
2013-06-13 12:41:25 -0500

    Added more programs to Longtermuse folder
    Makes it easier to send between computers

---
+++ MATLAB/LongTermUse/OrgCon.m
@@ -0,0 +1,6 @@
+cd C:\OrgCon\OrgConCode\MATLAB
+addpath('LongTermUse\'); %Adds subfolder to path
+
+set(0,'DefaultAxesColorOrder',morelines,'DefaultAxesLineStyleOrder','-|--|:')
+
+commonvars %Sets up many global constants
