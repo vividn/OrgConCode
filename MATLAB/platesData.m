2ae5e2d1fe218bdfd91dc06a30cfdfdb8cd5dea5
Nate Faber <fabe0088@umn.edu>
2013-06-13 11:54:11 -0500

    platesData functional now. May tweak later

--- MATLAB/platesData.m
+++ MATLAB/platesData.m
@@ -18,6 +18,39 @@ end
 % The times when the plating segment begin and end can be detected in the
 % left hand y-position very easily.
 leftHandY = SubjectStructure(1).Lh.y;
-beginTime = extremaSelector(leftHandY,'max',10000);
-endTime = extremaSelector(leftHandY,'max',14500);
+beginTime = extremaSelector(leftHandY,'max',15000);
+endTime = extremaSelector(leftHandY,'max',20000);
+
+plateRhPc = processData(SubjectStructure(1).Rh.glovePC,beginTime,endTime);
+cmpplot(plateRhPc)
+
+end %function plateData
+
+
+function outputData = processData(data,beginTime,endTime)
+% Chops the data at the specified begining and end points
+% and then concatenates and pads the result into a new matrix
+nTrials = min(size(beginTime,2),size(endTime,2));
+dim = find(size(data)==nTrials);
+if isempty(dim)
+    error('Number of trials and timepoints not equivalent')
+end
+
+% Initialize output variable (must start empty for cat purposes)
+outputData = [];
+
+% Add all the truncated data
+for iTrial = 1:nTrials
+    truncationIndices = beginTime(iTrial):endTime(iTrial);
+    
+    %Create a structure for custom indexing
+    S = struct;
+    S.type= '()';
+    S.subs = {truncationIndices,':',':'}; % Sets up the subs
+    S.subs{dim} = iTrial; % Replaces the correct index with the trial number
+    
+    truncatedData = subsref(data,S);
+    outputData = nancat(dim,outputData,truncatedData);
+end
+end %function processData
 
