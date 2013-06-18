6739ca6437452b96246cb34eab780d00dccaf9ed
Nate Faber <fabe0088@umn.edu>
2013-06-18 15:35:42 -0500

    Changed all subjects to 2 initials

--- MATLAB/commonvars.m
+++ MATLAB/commonvars.m
@@ -8,7 +8,7 @@ HOME_DIR = [ROOT_DIR 'OrgConCode\MATLAB\'];
 DATA_DIR = [ROOT_DIR 'ExperimentData\'];
 ANALYSIS_DIR = [ROOT_DIR 'Analysis\'];
 
-SUBJECTS = {'AAC','BSB','JCQ','JJT','JMS','MF3','NJF','PM'};
+SUBJECTS = {'AC','BB','JQ','JS','JT','MF','NF','PM'};
 
 %Joints is a structure that has information on the sensors in the
 %cyberglove.
