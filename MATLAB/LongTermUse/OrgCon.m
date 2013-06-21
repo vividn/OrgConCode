c25982a4f52db486b10d87267d2a9461791925fa
Nate Faber <fabe0088@umn.edu>
2013-06-21 13:03:09 -0500

    Automatically adds ANALYSIS_DIR to search path
    + subfolders. Allows for easy loading of data

--- MATLAB/LongTermUse/OrgCon.m
+++ MATLAB/LongTermUse/OrgCon.m
@@ -1,6 +1,12 @@
 cd C:\OrgCon\OrgConCode\MATLAB
-addpath('LongTermUse\'); %Adds subfolder to path
+commonvars %Sets up many global constants
 
+global ANALYSIS_DIR
+addpath('LongTermUse\'); %Adds subfolder to path
+analysisFolders = genpath(ANALYSIS_DIR); %Includes all subfolders
+addpath(analysisFolders);
+
+% Changes default plotting of lines to be more colorful
 set(0,'DefaultAxesColorOrder',morelines,'DefaultAxesLineStyleOrder','-|--|:')
 
-commonvars %Sets up many global constants
+clearvars;
