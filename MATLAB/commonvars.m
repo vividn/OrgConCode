d96520d28b145f0d7da458aea2056bf18f93b37c
Nate Faber (Laptop) <fabe0088@umn.edu>
2013-07-01 03:23:03 -0500

    Put principal component values in separate folder

--- MATLAB/commonvars.m
+++ MATLAB/commonvars.m
@@ -44,8 +44,8 @@ Joints.WRIST = [16 17];
 
 
 % Principal Components to use as a default
-PC_AVG = importdata([ANALYSIS_DIR 'PC_AVG.mat']);
-PC_COEFFS = importdata([ANALYSIS_DIR 'PC_COEFFS.mat']);
+PC_AVG = importdata([ANALYSIS_DIR '\PC\' 'PC_AVG.mat']);
+PC_COEFFS = importdata([ANALYSIS_DIR '\PC\' 'PC_COEFFS.mat']);
 
 % Only use the first 3 PC's
 PC_COEFFS = PC_COEFFS(1:3,:);
