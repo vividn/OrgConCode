f2e93c619a41d7dcd9b13e153d03a6782b81a40f
Nate Faber (Laptop) <fabe0088@umn.edu>
2013-07-01 02:36:39 -0500

    Changed dimshift to include destination dimension

--- MATLAB/LongTermUse/dimshift.m
+++ MATLAB/LongTermUse/dimshift.m
@@ -1,33 +1,41 @@
-function permutedMatrix = dimshift(inputMatrix,dim)
+function permutedMatrix = dimshift(inputMatrix,dim,newDim)
 % Permutes the specified dimension to the front of the matrix leaving the
 % rest in order.
 % For some operations with varying numbers of dimensions in the input, it
 % can be useful to move a dimension of interest to the begining for
 % coding's sake.
 %
-% Inputs: inputMatrix, any matrix with n-dimensions
-% dim = the dimension to permute to the begining.
-% If dim is negative, the function permutes the specified dimension back
-% into its original place.
+% Inputs:
+% inputMatrix, any matrix with n-dimensions
+%
+% dim = the dimension to permute to the location of newDim
+%
+% newDim = the new location for the dimension to reside. Default: 1
+%     (the beginning)
 %
 % Outputs: permutedMatrix, the new matrix with the dimensions rearranged
 
 nDims = ndims(inputMatrix);
 
 % Check for input validity
+if nargin < 3
+    newDim = 1;
 if nargin < 2
     error('Not Enough Inputs')
 end
+end
 
-if or(dim == 0, abs(dim) > nDims)
+if dim <= 0 || newDim <= 0 || dim > nDims || newDim >nDims
     error('Invalid dimension argument')
 end
 
-if dim > 0
-    newDimOrder = [dim, 1:(dim-1), (dim+1):nDims];
-elseif dim < 0
-    dim = -dim;
-    newDimOrder = [2:dim, 1, (dim+1):nDims];
+if newDim > dim
+    newDimOrder = [1:(dim-1), (dim+1):newDim, dim, (newDim+1):nDims];
+elseif newDim < dim
+    newDimOrder = [1:(newDim-1), dim, newDim:(dim-1), (dim+1):nDims];
+else
+    % Dimensions should be the same
+    newDimOrder = 1:nDims;
 end
 
 permutedMatrix = permute(inputMatrix,newDimOrder);
